d6d33b6dc962b9c0598025a88a6c3443d461db20
mweuro <20mehow01@gmail.com>
2025-11-17 17:33:47 +0100

    modify bash script

--- download_dataset.sh
+++ download_dataset.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 curl -L -o animals10.zip https://www.kaggle.com/api/v1/datasets/download/alessiocorrado99/animals10 && \
-unzip -q animals10.zip -d data && \
+unzip -q animals10.zip -d . && \
 rm animals10.zip
-
-echo "Dataset downloaded and extracted to 'data' directory."
+mv translate.py src/translate.py
+echo "Dataset downloaded!"
---
+++ src/split.py
@@ -0,0 +1,9 @@
+import sys
+import os
+import shutil
+import random
+
+sys.path.append(os.path.join(os.path.dirname(__file__), "..", "data"))
+from data.translate import translate
+
+print(translate)
